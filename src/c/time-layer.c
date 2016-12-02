183629ebb8fb39d1a146ceb4e3f5ee8e4a2d05b1
David Morgan <dmorgan81@gmail.com>
2016-12-02 15:10:13 -0500

    Live settings

--- src/c/time-layer.c
+++ src/c/time-layer.c
@@ -10,27 +10,39 @@ typedef struct {
     char buf[8];
     FFont *font;
     EventHandle tick_timer_event_handle;
+    EventHandle settings_event_handle;
 } Data;
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed, void *this) {
+    log_func();
+    Data *data = fctx_layer_get_data(this);
+    if (enamel_get_TIME_LEADING_ZERO()) {
+        strftime(data->buf, sizeof(data->buf), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
+    } else {
+        strftime(data->buf, sizeof(data->buf), clock_is_24h_style() ? "%k:%M" : "%l:%M", tick_time);
+    }
+    layer_mark_dirty(this);
+}
+
+static void settings_handler(void *this) {
+    log_func();
+    time_t now = time(NULL);
+    tick_handler(localtime(&now), MINUTE_UNIT, this);
+}
 
 static void update_proc(FctxLayer *this, FContext* fctx) {
     log_func();
     Data *data = fctx_layer_get_data(this);
     FRect bounds = fctx_layer_get_bounds(this);
     FPoint center = FPoint(bounds.size.w / 2, bounds.size.h / 2);
+    FPoint offset = FPoint(INT_TO_FIXED(enamel_get_TIME_X()), INT_TO_FIXED(enamel_get_TIME_Y()));
 
-    fctx_set_offset(fctx, center);
-    fctx_set_text_em_height(fctx, data->font, 40);
-    fctx_set_fill_color(fctx, GColorWhite);
+    fctx_set_offset(fctx, fpoint_add(center, offset));
+    fctx_set_text_em_height(fctx, data->font, enamel_get_TIME_FONT_SIZE());
+    fctx_set_fill_color(fctx, enamel_get_TIME_COLOR());
     fctx_begin_fill(fctx);
-    fctx_draw_string(fctx, data->buf, data->font, GTextAlignmentCenter, FTextAnchorMiddle);
+    fctx_draw_string(fctx, data->buf, data->font, atoi(enamel_get_TIME_ALIGNMENT()), FTextAnchorMiddle);
     fctx_end_fill(fctx);
-}
-
-static void tick_handler(struct tm *tick_time, TimeUnits units_changed, void *this) {
-    log_func();
-    Data *data = fctx_layer_get_data(this);
-    strftime(data->buf, sizeof(data->buf), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
-    layer_mark_dirty(this);
 }
 
 TimeLayer *time_layer_create() {
@@ -45,12 +57,15 @@ TimeLayer *time_layer_create() {
     tick_handler(localtime(&now), MINUTE_UNIT, this);
     data->tick_timer_event_handle = events_tick_timer_service_subscribe_context(MINUTE_UNIT, tick_handler, this);
 
+    data->settings_event_handle = enamel_settings_received_subscribe(settings_handler, this);
+
     return this;
 }
 
 void time_layer_destroy(TimeLayer *this) {
     log_func();
     Data *data = fctx_layer_get_data(this);
+    enamel_settings_received_unsubscribe(data->settings_event_handle);
     events_tick_timer_service_unsubscribe(data->tick_timer_event_handle);
     fctx_layer_destroy(this);
 }
--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -1,3 +1,28 @@
+var liveconfig = require('./liveconfig');
+
 var Clay = require('pebble-clay');
 var clayConfig = require('./config.json');
-var clay = new Clay(clayConfig);
+var customClay = require('./custom-clay');
+var clay = new Clay(clayConfig, customClay, {autoHandleEvents: false});
+
+Pebble.addEventListener('showConfiguration', function(e) {
+  var settings = JSON.parse(localStorage.getItem('clay-settings')) || {};
+  liveconfig.connect(Pebble.getAccountToken(), function(id, value) {
+    settings[id] = value;
+    Pebble.sendAppMessage(Clay.prepareSettingsForAppMessage(settings));
+  });
+
+  Pebble.openURL(clay.generateUrl());
+});
+
+Pebble.addEventListener('webviewclosed', function(e) {
+  if (e && !e.response) { return; }
+
+  // Send settings to Pebble watchapp
+  Pebble.sendAppMessage(clay.getSettings(e.response), function(e) {
+    console.log('Sent config data to Pebble');
+  }, function() {
+    console.log('Failed to send config data!');
+    console.log(JSON.stringify(e));
+  });
+});
