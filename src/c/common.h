836d5299bb490996fe2a671d0cc38fdfaa92e478
David Morgan <dmorgan81@gmail.com>
2016-12-02 13:55:19 -0500

    Settings framework

--- src/c/common.h
+++ src/c/common.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <pebble.h>
 #include "logging.h"
+#include "enamel.h"
 
 #ifdef PBL_PLATFORM_EMERY
 #define PBL_DISPLAY_LARGE
@@ -11,8 +12,3 @@
 #define PBL_IF_DISPLAY_LARGE_ELSE(if_true, if_false) (if_false)
 #define PBL_IF_DISPLAY_SMALL_ELESE(if_true, if_false) (if_true)
 #endif
-
-/*
-#define PBL_DISPLAY_WIDTH PBL_IF_ROUND_ELSE(180, PBL_IF_DISPLAY_LARGE_ELSE(200, 144))
-#define PBL_DISPLAY_HEIGHT PBL_IF_ROUND_ELSE(180, PBL_IF_DISPLAY_LARGE_ELSE(228, 168))
-*/
--- src/c/constructor.c
+++ src/c/constructor.c
@@ -1,4 +1,7 @@
 #include "common.h"
+#include <pebble-events/pebble-events.h>
+#include <pebble-connection-vibes/connection-vibes.h>
+#include <pebble-hourly-vibes/hourly-vibes.h>
 #include "fonts.h"
 #include "fctx-layer.h"
 #include "time-layer.h"
@@ -8,17 +11,30 @@ static Window *s_window;
 static FctxLayer *s_root_layer;
 static TimeLayer *s_time_layer;
 
+static EventHandle s_settings_event_handle;
+
+static void settings_handler(void *context) {
+    log_func();
+    window_set_background_color(s_window, enamel_get_BACKGROUND_COLOR());
+    connection_vibes_set_state(atoi(enamel_get_CONNECTION_VIBE()));
+    hourly_vibes_set_enabled(enamel_get_HOURLY_VIBE());
+}
+
 static void window_load(Window *window) {
     log_func();
-    window_set_background_color(window, GColorBlack);
     s_root_layer = fctx_layer_get_root_layer(window);
 
     s_time_layer = time_layer_create();
     fctx_layer_add_child(s_root_layer, s_time_layer);
+
+    settings_handler(NULL);
+    s_settings_event_handle = enamel_settings_received_subscribe(settings_handler, NULL);
 }
 
 static void window_unload(Window *window) {
     log_func();
+    enamel_settings_received_unsubscribe(s_settings_event_handle);
+
     time_layer_destroy(s_time_layer);
     fctx_layer_destroy(s_root_layer);
 }
@@ -26,6 +42,21 @@ static void window_unload(Window *window) {
 static void init(void) {
     log_func();
     fonts_init();
+    enamel_init();
+    connection_vibes_init();
+    hourly_vibes_init();
+    uint32_t const pattern[] = { 100 };
+    hourly_vibes_set_pattern((VibePattern) {
+        .durations = pattern,
+        .num_segments = 1
+    });
+
+#ifdef PBL_HEALTH
+    connection_vibes_enable_health(true);
+    hourly_vibes_enable_health(true);
+#endif
+
+    events_app_message_open();
 
     s_window = window_create();
     window_set_window_handlers(s_window, (WindowHandlers) {
@@ -39,6 +70,9 @@ static void deinit(void) {
     log_func();
     window_destroy(s_window);
 
+    hourly_vibes_deinit();
+    connection_vibes_deinit();
+    enamel_deinit();
     fonts_deinit();
 }
 
