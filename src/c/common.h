19a8c85124613ef7db3ef914c28cd61d88200233
David Morgan <dmorgan81@gmail.com>
2017-03-17 11:06:55 -0400

    Remove emery support
    Oh what could have been

--- src/c/common.h
+++ src/c/common.h
@@ -2,13 +2,3 @@
 #include <pebble.h>
 #include "logging.h"
 #include "enamel.h"
-
-#ifdef PBL_PLATFORM_EMERY
-#define PBL_DISPLAY_LARGE
-#define PBL_IF_DISPLAY_LARGE_ELSE(if_true, if_false) (if_true)
-#define PBL_IF_DISPLAY_SMALL_ELESE(if_true, if_false) (if_false)
-#else
-#define PBL_DISPLAY_SMALL
-#define PBL_IF_DISPLAY_LARGE_ELSE(if_true, if_false) (if_false)
-#define PBL_IF_DISPLAY_SMALL_ELESE(if_true, if_false) (if_true)
-#endif
