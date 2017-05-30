941fc3509ddf3745eb7c59753b342acee32d7876
David Morgan <dmorgan81@gmail.com>
2017-05-30 14:07:12 -0400

    Create aplite specific font
    Memory on aplite is right up to the edge. Additional characters in the
    font file causes aplite to crash when receiving settings.

--- regen_font.sh
+++ regen_font.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+node_modules/.bin/fctx-compiler assets/LECO1976-Regular.svg -r "[0-9a-zA-Z \-/:%.]"
+mv resources/LECO1976-Regular.ffont resources/LECO1976-Regular~aplite.ffont
 node_modules/.bin/fctx-compiler assets/LECO1976-Regular.svg -r "[0-9a-zA-Z \-\\/:%°.\[\](){}<>]"
